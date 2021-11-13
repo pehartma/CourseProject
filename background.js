36c83cc1c0ba1042ab8f6354a90161de4c72d1ed
Steven Hernandez <stevenkohta@uchicago.edu>
2021-11-13 17:27:30 -0600

    Filled in most of replaceTextInNode() -- regex to match top ranked tokens in document missing

--- background.js
+++ background.js
@@ -38,16 +38,76 @@ function replaceTextInNode (parentNode){
     //for every bag of words token given by loadPageTokens, the "word" needs to be replaced with
     // "<a href=http//google.com/?word>word</a>"
     // or something like that.
-    for(var i = parentNode.childNodes.length-1; i >= 0; i--){
-        var node = parentNode.childNodes[i];
 
-        if(node.nodeType == Element.TEXT_NODE){
-            node.textContent = node.textContent.replace('shade', 'shade-markup');
-        } else if (node.nodeType == Element.ELEMENT_NODE){
-            replaceTextInNode(node);
+    // for(var i = parentNode.childNodes.length-1; i >= 0; i--){
+    //     var node = parentNode.childNodes[i];
+
+    //     if(node.nodeType == Element.TEXT_NODE){
+    //         node.textContent = node.textContent.replace('shade', 'shade-markup');
+    //     } else if (node.nodeType == Element.ELEMENT_NODE){
+    //         replaceTextInNode(node);
+    //     }
+    // }
+
+    var regex = /shade/g; // TODO: Create regex that matches all the top K ranked tokens
+    matchText(parentNode, regex, function(node, match, offset) {
+        var a = document.createElement("a");
+        var preprocessQuery = function(queryText) {
+            queryText = queryText.
+                trim().
+                replace(/\s+/g, "+");
+            return encodeURIComponent(queryText).
+                replace(/['()]/g, escape).
+                replace(/\*/g, '%2A');
+        };
+        switch(engineLocalValue) {
+            case 0: // Google link
+                a.href = "https://www.google.com/search?q=" + preprocessQuery(match);
+                break;
+            case 1: // Wikipedia link
+                a.href = "https://en.wikipedia.org/w/index.php?search=" + preprocessQuery(match);
+                break;
         }
-    }
+        a.textContent = match;
+        return a;
+    });
 }
+
+function matchText(node, regex, callback, excludeElements) {
+    // Original Source: https://stackoverflow.com/questions/16662393/insert-html-into-text-node-with-javascript
+
+    excludeElements || (excludeElements = ['a', 'script', 'style', 'iframe', 'canvas']);
+    var child = node.firstChild;
+
+    while (child) {
+        switch (child.nodeType) {
+        case 1:
+            if (excludeElements.indexOf(child.tagName.toLowerCase()) > -1)
+                break;
+            matchText(child, regex, callback, excludeElements);
+            break;
+        case 3:
+            var bk = 0;
+            child.data.replace(regex, function(all) {
+                var args = [].slice.call(arguments),
+                    offset = args[args.length - 2],
+                    newTextNode = child.splitText(offset+bk), tag;
+                bk -= child.data.length + all.length;
+
+                newTextNode.data = newTextNode.data.substr(all.length);
+                tag = callback.apply(window, [child].concat(args));
+                child.parentNode.insertBefore(tag, newTextNode);
+                child = newTextNode;
+            });
+            regex.lastIndex = 0;
+            break;
+        }
+
+        child = child.nextSibling;
+    }
+
+    return node;
+};
 
 
 loadPageTokens()
